3db95ea0e29a6501e27ce010583a0cb42e36f098
rtonata88 <rtonata@gmail.com>
2022-01-10 21:32:47 +0200

    Add styling and basic functionality

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-function TodosList(props) {
-  return (
-    <ul>
-      {props.todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}{' '}
-    </ul>
-  );
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+          />
+        ))}{' '}
+      </ul>
+    );
+  }
 }
 
 export default TodosList;
